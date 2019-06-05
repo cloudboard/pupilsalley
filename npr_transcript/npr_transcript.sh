88a80da1d3a2b4d22b9af513f1f1ad019eaa345b
brook hong <zghong@amazon.com>
2019-06-05 23:45:03 +0000

    npr_transcript: 2019-06-05

--- npr_transcript/npr_transcript.sh
+++ npr_transcript/npr_transcript.sh
@@ -2,11 +2,11 @@
 today=$(date +%Y-%m-%d)
 /home/zghong/.local/python-3.6.3/bin/scrapy runspider /home/zghong/.servant/npr_transcript/npr_transcript.py -o /home/zghong/.servant/npr_transcript/data/$today.json > /dev/null 2>&1
 
-if [ -f "/home/zghong/.servant/npr_transcript/data/$today.json" ]; then
+if [ -s "/home/zghong/.servant/npr_transcript/data/$today.json" ]; then
     cd /home/zghong/.servant/
 
     cp npr_transcript/data/$today.json npr_transcript/data/latest.json
     git add .
     git commit -m "npr_transcript: $today"
-    GIT_SSH_COMMAND="ssh -i ~/.ssh/id_cloudboard" git push > /dev/null
+    GIT_SSH_COMMAND="ssh -i ~/.ssh/id_cloudboard" git push > /dev/null 2>&1
 fi
